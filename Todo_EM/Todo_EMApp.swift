9956c0e4fc5f11484d8fd318ff38810ff217c207
Matvey <metrloopod2121@mail.ru>
2025-01-24 21:32:36 +0300

    Initial Commit

---
+++ Todo_EM/Todo_EMApp.swift
@@ -0,0 +1,20 @@
+//
+//  Todo_EMApp.swift
+//  Todo_EM
+//
+//  Created by 𝕄𝕒𝕥𝕧𝕖𝕪 ℙ𝕠𝕕𝕘𝕠𝕣𝕟𝕚𝕪 on 24.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct Todo_EMApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
---
+++ Todo_EMTests/Todo_EMTests.swift
@@ -0,0 +1,17 @@
+//
+//  Todo_EMTests.swift
+//  Todo_EMTests
+//
+//  Created by 𝕄𝕒𝕥𝕧𝕖𝕪 ℙ𝕠𝕕𝕘𝕠𝕣𝕟𝕚𝕪 on 24.01.2025.
+//
+
+import Testing
+@testable import Todo_EM
+
+struct Todo_EMTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
